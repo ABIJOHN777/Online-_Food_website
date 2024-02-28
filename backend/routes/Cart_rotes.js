db94091d1e225fc1541c2b50c9a1bf77a7617a31
ABIJOHN777 <158270813+ABIJOHN777@users.noreply.github.com>
2024-02-28 13:23:37 +0530

    Add files via upload

--- backend/routes/Cart_rotes.js
+++ backend/routes/Cart_rotes.js
@@ -1,4 +1,26 @@
 import express from 'express'
+import { CartModel } from '../model/cart_model.js'
 
 const router = express.Router()
 
+router.post("/cart",async(req,res)=>{
+    const{ownerID,cartItems}=req.body
+    console.log(cartItems)
+    try{
+        const newCart=await new CartModel({ownerID,products:cartItems})
+       if(newCart)
+       {
+       await newCart.save()
+       return res.json({message:"successfull"})
+    }
+       else{
+        return res.json({message:"cannot add to database"})
+       }
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+
+export { router as CartRouter}
